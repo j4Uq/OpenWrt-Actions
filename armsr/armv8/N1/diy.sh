3d9fdd76f632292b35846ed82d202c9449903477
j4Uq <6436073+j4Uq@users.noreply.github.com>
2026-06-29 20:26:42 +0800

    adjust release path

--- armsr/armv8/N1/diy.sh
+++ armsr/armv8/N1/diy.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-# Adjust source code
-patch -p1 -f < $(dirname "$0")/luci.patch
-
 # Add packages
 git clone https://github.com/ophub/luci-app-amlogic --depth=1 clone/amlogic
 cp -rf clone/amlogic/luci-app-amlogic feeds/luci/applications/
